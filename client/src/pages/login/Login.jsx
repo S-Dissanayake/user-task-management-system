d1e635117d3784a399a6a7e614fc2156c0714fcd
S Dissanyake <89515541+S-Dissanayake@users.noreply.github.com>
2024-04-05 19:10:29 +0530

    logo removed from login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -193,14 +193,6 @@ const Login = () => {
         height: 1,
       }}
     >
-      <Logo
-        sx={{
-          position: 'fixed',
-          top: { xs: 16, md: 24 },
-          left: { xs: 16, md: 24 },
-        }}
-      />
-
       <Stack alignItems="center" justifyContent="center" sx={{ height: 1 }}>
         <Card
           sx={{
