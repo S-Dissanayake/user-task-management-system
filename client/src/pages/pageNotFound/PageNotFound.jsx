290a4d1b4125706fd8e5a45d8cce095a2a812196
S Dissanyake <89515541+S-Dissanayake@users.noreply.github.com>
2024-04-05 21:03:12 +0530

    updated PageNotFound  with new UI

--- client/src/pages/pageNotFound/PageNotFound.jsx
+++ client/src/pages/pageNotFound/PageNotFound.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
-const PageNotFound = () => {
+import { RouterLink } from '../../routes/components';
+
+import PageNotFound from "../../assets/404-page-not-found.png";
+
+// ----------------------------------------------------------------------
+
+export default function NotFoundView() {
+
+
   return (
-    <div>PageNotFound</div>
-  )
-}
+    <>
+      <Container>
+        <Box
+          sx={{
+            py: 12,
+            maxWidth: 480,
+            mx: 'auto',
+            display: 'flex',
+            minHeight: '100vh',
+            textAlign: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography variant="h3" sx={{ mb: 3 }}>
+            Sorry, page not found!
+          </Typography>
+          <Box
+            component="img"
+            src={PageNotFound}
+            sx={{
+              mx: 'auto',
+              height: 260,
+              my: { xs: 5, sm: 10 },
+            }}
+          />
 
-export default PageNotFound
+          <Button href="/" size="large" variant="contained" component={RouterLink}>
+            Go to Login
+          </Button>
+        </Box>
+      </Container>
+    </>
+  );
+}
