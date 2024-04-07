9f96ef042fe9f0b86b3164059e3ea7a929b85659
S Dissanyake <89515541+S-Dissanayake@users.noreply.github.com>
2024-04-07 22:37:35 +0530

    added id's for testing

--- client/src/pages/dashboard/table-row.jsx
+++ client/src/pages/dashboard/table-row.jsx
@@ -73,17 +73,17 @@ export default function UserTableRow({
 
         <TableCell align="right">
         <Tooltip title="View">
-            <IconButton onClick={()=>{handleActionClick("VIEW")}} sx={{backgroundColor: '#e0e0e0', ":hover":{backgroundColor: '#c9c7c7'}, marginRight: "15px"}}>
+            <IconButton data-testid='view-btn' onClick={()=>{handleActionClick("VIEW")}} sx={{backgroundColor: '#e0e0e0', ":hover":{backgroundColor: '#c9c7c7'}, marginRight: "15px"}}>
               <img src={ViewSvg} alt='view' width="20px" />  
             </IconButton>
           </Tooltip>
           <Tooltip title="Edit">
-            <IconButton onClick={()=>{handleActionClick("EDIT")}} sx={{backgroundColor: '#b7d8f7', ":hover":{backgroundColor: '#93c8f9'}, marginRight: "15px"}}>
+            <IconButton data-testid='edit-btn' onClick={()=>{handleActionClick("EDIT")}} sx={{backgroundColor: '#b7d8f7', ":hover":{backgroundColor: '#93c8f9'}, marginRight: "15px"}}>
               <img src={EditSvg} alt='edit' width="20px"/>    
             </IconButton>
           </Tooltip>
           <Tooltip title="Delete">
-            <IconButton onClick={()=>{handleActionClick("DELETE")}} sx={{backgroundColor: '#fcbdbd', ":hover":{backgroundColor: '#f78f8f'}}}>
+            <IconButton data-testid='delete-btn' onClick={()=>{handleActionClick("DELETE")}} sx={{backgroundColor: '#fcbdbd', ":hover":{backgroundColor: '#f78f8f'}}}>
               <img src={DeleteSvg} alt='delete' width="20px"/>
             </IconButton>
           </Tooltip>
